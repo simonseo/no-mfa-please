d0c8b3959c4f51f689118553dad561709a17b57b
Simon Myunggun Seo <ms9144@nyu.edu>
2019-09-18 02:15:06 +0900

    change db configs

--- app/db/sql_config.py
+++ app/db/sql_config.py
@@ -4,29 +4,35 @@
 # @Created:   2018-04-17 14:14:28  Simon Myunggun Seo (simon.seo@nyu.edu) 
 # @Updated:   2018-04-18 19:10:56  Simon Seo (simon.seo@nyu.edu)
 import os
-import logging
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+from app import app
+
+on_heroku = 'DYNO' in os.environ
+on_local = not on_heroku # Is there a better way of checking if environment is local
+db_url = os.environ['DATABASE_URL']
 
 def get_config():
-	on_heroku = 'DYNO' in os.environ
-	on_local = not on_heroku # Is there a better way of checking if environment is local
+
 	if on_local:
-		return {
+		config = {
 			"host" : "127.0.0.1",
 			"port" : 5432,
 			"dbname" : "postgres",
+			"sslmode" : 'prefer',
 		}
+		app.logger.debug("config: {}".format(config))
+		return config
+
 	elif on_heroku:
 		config = {
-			"user" : os.environ['DATABASE_URL'].split('://')[1].split(':')[0],
-			"password" : os.environ['DATABASE_URL'].split(':')[1].split('@')[0],
-			"host" : os.environ['DATABASE_URL'].split('@')[1].split(':')[0],
-			"port" : int(os.environ['DATABASE_URL'].split(':')[-1].split('/')[0]),
-			"dbname" : os.environ['DATABASE_URL'].split('/')[-1],
+			"user" : db_url.split('://')[1].split(':')[0],
+			"password" : db_url.split(':')[1].split('@')[0],
+			"host" : db_url.split('@')[1].split(':')[0],
+			"port" : int(db_url.split(':')[-1].split('/')[0]),
+			"dbname" : db_url.split('/')[-1],
 			"sslmode" : 'require',
 		}
-		logger.debug("config: {}".format(config))
+		app.logger.debug("config: {}".format(config))
 		return config
+		
 	else:
 		raise Exception("Unknown environment; Neither Heroku or Local")
--- run.py
+++ run.py
@@ -6,6 +6,7 @@
 import os
 from app import app
 
-print(os.environ.keys())
+# print(os.environ.keys())
+debug = os.environ['FLASK_DEBUG']
 app.secret_key = "Some key"
-app.run(host="0.0.0.0", port=int(os.environ["PORT"]))
+app.run(host="0.0.0.0", port=int(os.environ["PORT"]), debug=debug)
