88371bef84d3f22688bf4d5758175bc7506addac
Simon Myunggun Seo <ms9144@nyu.edu>
2019-09-18 02:28:06 +0900

    fix db authentication password

--- app/db/sql_config.py
+++ app/db/sql_config.py
@@ -10,29 +10,28 @@ on_heroku = 'DYNO' in os.environ
 on_local = not on_heroku # Is there a better way of checking if environment is local
 db_url = os.environ['DATABASE_URL']
 
+local_config = {
+	"host" : "127.0.0.1",
+	"port" : 5432,
+	"dbname" : "postgres",
+	"sslmode" : 'prefer',
+}
+
+heroku_config = {
+	"user" : db_url.split('://')[1].split(':')[0],
+	"password" : db_url.split(':')[2].split('@')[0],
+	"host" : db_url.split('@')[1].split(':')[0],
+	"port" : int(db_url.split(':')[-1].split('/')[0]),
+	"dbname" : db_url.split('/')[-1],
+	"sslmode" : 'require',
+}
+
 def get_config():
-
 	if on_local:
-		config = {
-			"host" : "127.0.0.1",
-			"port" : 5432,
-			"dbname" : "postgres",
-			"sslmode" : 'prefer',
-		}
-		app.logger.debug("config: {}".format(config))
-		return config
-
+		app.logger.debug("config: {}".format(local_config))
+		return local_config
 	elif on_heroku:
-		config = {
-			"user" : db_url.split('://')[1].split(':')[0],
-			"password" : db_url.split(':')[1].split('@')[0],
-			"host" : db_url.split('@')[1].split(':')[0],
-			"port" : int(db_url.split(':')[-1].split('/')[0]),
-			"dbname" : db_url.split('/')[-1],
-			"sslmode" : 'require',
-		}
-		app.logger.debug("config: {}".format(config))
-		return config
-		
+		app.logger.debug("config: {}".format(heroku_config))
+		return heroku_config
 	else:
 		raise Exception("Unknown environment; Neither Heroku or Local")
